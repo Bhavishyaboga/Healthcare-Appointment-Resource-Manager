8f1f6a8d56cb0472c3c8a27aba9703470a3b2843
Bibekbehera28 <Bibekbehera283@gmail.com>
2024-06-08 16:41:37 +0530

    Uploaded Files

---
+++ healthcareProject/models.py
@@ -0,0 +1,7 @@
+# You might not need models for a simple login page, but here's a placeholder
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
